732137d8f4cbd12cf1e23b2aa22e955698c317e9
ultrabook notitle <ultrabook@nothis.com>
2015-10-20 15:13:56 +0800

    get_dir_or_exit inproving..

--- scripts/tests/test_get_dir_or_exit.sh
+++ scripts/tests/test_get_dir_or_exit.sh
@@ -40,9 +40,33 @@ huali
 rm  "${ABSROOT}""/temp/else11/else111/token"
 help_find_result=$( _help_of_find )
 echotest "$help_find_result"
+huali
 
 # homekeeping
 rm -rf temp/else11
+
+#######
+##  function: _actively()
+#######
+# func base on while forloop,now output simple
+#echotest "$(_actively)"
+#huali
+
+#global variable: step
+step=4
+
+# we wish it can no output,but return code example "0" "1"...
+sleep 1
+mkdir -p "${ABSROOT}""/temp/else11"
+touch  "${ABSROOT}""/temp/else11/token"
+_actively
+returncode=$?
+test $returncode -eq 0 && echotest "return 0"
+
+# do cleanning
+rm -rf "${ABSROOT}""/temp/else11/"
+
+
 
 
 
