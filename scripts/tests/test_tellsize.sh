bb03281d00d9d80ae93cfb87aa1aae918a51eb40
macbook_rasp2 <maybe.macbook@icloud.com>
2015-11-12 09:13:33 +0800

    fixed test logos:test_tellsize,notice string eq string in iisr

--- scripts/tests/test_tellsize.sh
+++ scripts/tests/test_tellsize.sh
@@ -10,11 +10,23 @@ project_dir=$( cd  $( dirname $0 ) &&  cd ../../ && pwd -P )
 # 1st test
 echotest "1st test,give a big file as \$1"
 thesize="$(tellsize ${FUNCPATH}/abigfile)"
-iisr "$thesize=more"
+#test "$thesize" = "more" && echo "::TRUE:: ""${thesize}=""more" || echo "::ERROR:: ""${thesize}=""more"
+iisr "'$thesize' = 'more'"
 huali
 
 # 2nd test
 echotest "2nd test,give a small file as \$1"
 thesize="$(tellsize ${FUNCPATH}/checkvolidor.sh)"
-iisr "$thesize=less"
+#test "$thesize" = "less" && echo "::TRUE:: ""${thesize}=""less" || echo "::ERROR:: ""${thesize}=""less"
+iisr "'$thesize' = 'less'"
+huali
 
+# 3nd test
+echotest "3nd test,for link type."
+ln -s ${FUNCPATH}/abigfile ${FUNCPATH}/link_to_abigfile
+thesize="$(tellsize ${FUNCPATH}/link_to_abigfile)"
+#test "$thesize" = "more" && echo "::TRUE:: ""${thesize}=""more" || echo "::ERROR:: ""${thesize}=""more"
+iisr "'$thesize' = 'more'"
+
+# home keeping
+rm -rf ${FUNCPATH}/link_to_abigfile
