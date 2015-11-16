b12e399aa091d9d3ce5818cacc8b0ac15022d9b5
macbook_rasp2 <maybe.macbook@icloud.com>
2015-11-16 15:26:18 +0800

    change type,create a main sh:scripts/saltit.sh

--- scripts/functions/actively.sh
+++ scripts/functions/actively.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # filename:actively.sh
 # refact at 2015-10-19 AM 9:47:00
-# actively.sh==main.sh(means:delete old contents of saltit.sh,let it be a soft link of this.)
+# actively.sh's actively()==saltit.sh's main()(means:delete old contents of saltit.sh,let it be a soft link of this.)
 function actively(){
 	# results keeps into the variable "founds"
 	founds=$( help_of_find )
@@ -9,8 +9,25 @@ function actively(){
 		echo "need uploading directory,has no found."
 		return 2
 	fi
-	#echo "ready for uploading,choice the number,\"3\" for exit."
 
+	declare usepass="00"
+	echo 'the first step:choice a simple pass,defaul is "00".'
+	select decide in "00 (default)" "i_want_input_mine:" 
+	do
+		case $REPLY in
+		2)
+			read -p "enter your simple password,like '0','1','01'..." usepass 
+			break
+			;;
+		*)
+			# means use default '00' as $usepass.
+			break
+			;;
+		esac
+	done
+			
+
+	echo 'the second step:use default directory or else?'
 	select decide in "${founds}"" (default)" "i_want_input_mine:" "exit"
 	do
 		case $REPLY in
@@ -25,8 +42,8 @@ function actively(){
 
 			for eachfile in $filelist
 			do
-				#salteach $eachfile "$usepass"			
-				salteach $eachfile "00"			
+				salteach $eachfile "$usepass"			
+#				salteach $eachfile "00"			
 			done
 
 			break
@@ -41,8 +58,8 @@ function actively(){
 
 			for eachfile in $filelist
 			do
-				#salteach $eachfile "$usepass"			
-				salteach $eachfile "00"			
+				salteach $eachfile "$usepass"			
+			#	salteach $eachfile "00"			
 			done
 
 			break
--- scripts/saltit.sh
+++ scripts/saltit.sh
@@ -1,1 +1,17 @@
-/Users/apple/Workspace/saltit/scripts/functions/actively.sh
+#!/bin/bash
+# filename saltit.sh
+
+# first of first,include abs root(directory) name.
+# then start 'Quartet Limited'
+# get some global variable -- "FUNCPATH" "ABSROOT" "include_all" "echotest" ..
+project_dir=$( cd  $( dirname $0 ) &&  cd ../../ && pwd -P )
+#echo $project_dir
+
+#no like scripts/tests/test_XXX,the project_dir exceed the working dir.
+. $project_dir/saltit/conf/abs.conf
+. ${FUNCPATH}/base 2>/dev/null
+
+DEBUG=0
+
+actively
+echo 'Done.'
