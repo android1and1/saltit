a97c6f595e9ac093846aa952233d08ca89a08c85
ultrabook-Alan <alan@ultrabook.net>
2015-11-20 14:53:04 +0800

    adjusted(will adjust again  i guess) split_if_nessXXX

--- scripts/functions/split_if_ness.sh
+++ scripts/functions/split_if_ness.sh
@@ -6,25 +6,12 @@
 split_if_ness(){
 	# $1 is bigfile prepare to be splited
 	# $2 is a directory store splitted files.
-	sizereport="$(tellsize $1)"
-	if [ $sizereport=more ];then
-		[[ $DEBUG > 1 ]] && echotest "prepare splitting!"
-		declare basenameofdollar1=$( basename $1 )
-		split -b 4m "$1"
-
-		# rename right now!
-		for name in $( ls x* )
-		do
-			mv $name ${2}/${basenameofdollar1}${name:1}
-		done
-		 		
-		# show what files in temp directory. 
-		ls -lh ${2}/${basenameofdoallar1}	
-		sleep 3
-		
-	else
-		echo \$size==$size\.
-		echo "$DEBUGTITLE"" size is no big enough,no splitting." 
-	fi
+	declare basenameofdollar1=$( basename $1 )
+	split -b 4m "$1"
+	# rename right now!
+	for name in $( ls x* )
+	do
+		mv $name ${2}/${basenameofdollar1}${name:1}
+	done
 }
 #-------------Tropic of Capricorn
--- scripts/tests/test_split_if_ness.sh
+++ scripts/tests/test_split_if_ness.sh
@@ -3,11 +3,38 @@
 project_dir=$( cd  $( dirname $0 ) &&  cd ../../ && pwd -P )
 . $project_dir/conf/abs.conf
 . ${FUNCPATH}/base 2>/dev/null
+DEBUG=0
 
-TESTDIR="${FUNCPATH}/temp"
-mkdir -p $TESTDIR
-split_if_ness ${ABSROOT}/scripts/functions/abigfile ${TESTDIR}
+KEEPDIR=${ABSROOT}/temp/temp_keep
+mkdir -p $KEEPDIR
+bigfile=${ABSROOT}/scripts/functions/abigfile
+test -f $bigfile
+iisr "$? -eq 0"
+if test "$(tellsize $bigfile)" = "more"
+then
+	echotest 'meet a big file.'
+else
+	echotest 'no bigger than expected.'
+	exit 2
+fi
+split_if_ness ${ABSROOT}/scripts/functions/abigfile ${KEEPDIR}
+iisr "$? -eq 0"
+
+# 2nd
+echotest "2nd test,splitted-files amount,name-mode."
+find ${KEEPDIR} -type f | grep --quiet "abigfile"
+iisr "$? -eq 0"
+find ${KEEPDIR} -type f | grep --quiet "nobigfile"
+iisr "$? -ne 0"
+counts=$(find ${KEEPDIR} -type f | wc -l)
+iisr "$counts -eq 5"
+
+# 3nd
+echotest "3nd test,use UPLOADS dir,default one."
+split_if_ness ${ABSROOT}/scripts/functions/abigfile ${UPLOADS}
+count3=$(find ${UPLOADS} -type f -name "abigfile*" | wc -l)
+iisr "$count3 -eq 5"
 
 # do house keeping.
-rm -rf $TESTDIR
-echotest 'removed test data(s).'
+rm -rf $KEEPDIR
+rm -rf $UPLOADS/abigfile*
