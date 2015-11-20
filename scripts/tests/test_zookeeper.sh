a712d7d26b36e7adf2bf42bd8baeaf53258084b1
ultrabook-Alan <alan@ultrabook.net>
2015-11-20 13:48:18 +0800

    remove no more need test func,rename 1

--- scripts/tests/test_zookeeper.sh
+++ scripts/tests/test_zookeeper.sh
@@ -13,29 +13,18 @@ DEBUG=0
 TEMPDIR=${ABSROOT}/temp/temp14
 mkdir -p $TEMPDIR
 echo 'data here .' > $TEMPDIR/a.txt
-echo 'data here .' > $TEMPDIR/b.txt
-echo 'data here .' > $TEMPDIR/c.txt
-echo 'data here .' > $TEMPDIR/d.txt
-echo 'data here .' > $TEMPDIR/e.txt
+echo 'data here baby.' > $TEMPDIR/b.txt
+echo 'data here baby girl.' > $TEMPDIR/c.txt
 touch $TEMPDIR/.token
 echo 'a.txt' > $TEMPDIR/.ignores
 # create real data file(crypted)
 actively
-#ls ${UPLOADS}
-#sleep 1
-# see some lines
-#cat ${UPLOADS}/encoded_c.txt.data
 
 zookeeper $UPLOADS
+ls $DOWNLOADS | grep --quiet "decoded_encoded_c.txt"
 iisr "$? -eq 0"
-#filelists=$(ls $DOWNLOADS)
-#echotest "$filelists"
-# see some lines
-tail ${DOWNLOADS}/deco*
-huali
-
-#echotest "2nd test..."
-
+cat $DOWNLOADS/decoded_encoded_c.txt | grep --silent "baby girl"
+iisr "$? -eq 0"
 
 # home keeping
 sleep 1
