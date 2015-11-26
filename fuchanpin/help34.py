90c870b63bfbb5d579c9fb7ec6a2395ee12329c3
Hua Gong <huagongs2000@yahoo.com.cn>
2015-11-26 15:30:26 +0800

    in hp pc version,only py34 can use,now fixed this problem.

---
+++ fuchanpin/help34.py
@@ -0,0 +1,12 @@
+import sys
+import random
+if len(sys.argv) > 1:
+	rangenum=sys.argv[1]
+else:
+	rangenum=100	
+arr=[ x for x in range(int(rangenum)) ]
+random.shuffle(arr)
+print('(')
+for i in arr:
+	print(i)
+print(')')
